59f5ee147c60b6f4a7444dc27cae1d3694dec98b
Stephan Kergomard <webmaster@kergomard.ch>
2023-12-05 07:48:12 +0100

    Update Version to 1.4.2

--- plugin.php
+++ plugin.php
@@ -23,7 +23,7 @@
     $id = 'crevento';
 
     // Version Information of Plugin & compatible versions of ILIAS
-    $version = '1.4.1';
+    $version = '1.4.2';
     $ilias_min_version = '7.0';
     $ilias_max_version = '8.999';
 
